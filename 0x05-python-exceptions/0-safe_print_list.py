a9a717869cd98c21842afe3b5f4bbe49e1726b04
MamdouhEnahla <mamdouh57118@gmail.com>
2024-01-23 00:48:23 +0200

    out-of-index-edit

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,8 +2,9 @@
 def safe_print_list(my_list=[], x=0):
     i = 0
     try:
-        for i in range(x):
+        while i < x:
             print(my_list[i], end='')
+            i += 1
     except IndexError:
         None
     print()
