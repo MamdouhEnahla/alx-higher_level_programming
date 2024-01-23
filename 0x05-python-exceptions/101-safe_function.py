367679621d046d861bea688a2bdde0e93dc40966
MamdouhEnahla <mamdouh57118@gmail.com>
2024-01-23 02:11:18 +0200

    safe-fn-edit...

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -5,7 +5,7 @@ import sys
 def safe_function(fct, *args):
     res = None
     try:
-        res = fct(args[1:])
+        res = fct(*args)
     except Exception as ex:
         print("Exception:", ex, file=sys.stderr)
     return res
