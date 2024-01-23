fa4241495f140b7c57f541a3ee033bdb5a33f5b6
MamdouhEnahla <mamdouh57118@gmail.com>
2024-01-23 02:04:20 +0200

    typo-stderr...

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -5,7 +5,7 @@ import sys
 def safe_print_integer_err(value):
     res = True
     try:
-        print("{:d}".formatv(value))
+        print("{:d}".format(value))
     except Exception as ex:
         print("Exception:", ex, file=sys.stderr)
         res = False
