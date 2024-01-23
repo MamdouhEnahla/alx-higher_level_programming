f3019eeb5bbdbdb01987c5b20418fcd769d81c5f
MamdouhEnahla <mamdouh57118@gmail.com>
2024-01-23 06:56:36 +0200

    square-class2-edit...

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -6,9 +6,8 @@ class Square:
     """Square"""
 
     def __init__(self, size=0):
-        try:
-            self.__size = size
-        except TypeError as te:
-            raise te("size must be an integer")
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
+        self.__size = size
