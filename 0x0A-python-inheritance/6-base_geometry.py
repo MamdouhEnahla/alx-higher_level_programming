2c19cd242b2e516b94f94f01f256eaddb100e2a8
MamdouhEnahla <mamdouh57118@gmail.com>
2024-02-06 00:57:13 +0200

    base-geometry...

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """class with public function area"""
+    def area(self):
+        """raises an exception when called"""
+        raise Exception("area() is not implemented")
