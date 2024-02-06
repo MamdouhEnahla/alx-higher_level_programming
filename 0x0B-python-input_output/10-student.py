d34e15128b6f78ed44d8ad64c77813589fc0f767
MamdouhEnahla <mamdouh57118@gmail.com>
2024-02-06 15:44:16 +0200

    edit-json...

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,20 +1,19 @@
 #!/usr/bin/python3
-"""Student Module """
+"""student class"""
 
 
 class Student:
-    """Student class"""
+    """class stuff"""
     def __init__(self, first_name, last_name, age):
-        """ initializatoin """
+        """initializes"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """
-        Converts the Student object to a dictionary (JSON-like representation).
-        """
-        if isinstance(attrs, list) and all(isinstance(x, str) for x in attrs):
-            return {x: getattr(self, x) for x in attrs} if attrs
+        """prints __dict__"""
+        if (isinstance(attrs, list) and
+                all(isinstance(x, str) for x in attrs)):
+            return ({x: y for x, y in self.__dict__.items() if x in attrs})
         else:
             return self.__dict__
