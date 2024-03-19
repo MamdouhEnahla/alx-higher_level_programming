fc5e9b4d8a20db15474d46b20cf394ad2630611a
MamdouhEnahla <mamdouh57118@gmail.com>
2024-03-19 07:25:58 +0200

    create-if...

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,23 +1,3 @@
-import mysql.connector
-
-def create_database(database_name):
-    try:
-        connection = mysql.connector.connect(
-            host="localhost",
-            user="your_username",
-            password="your_password"
-        )
-
-        cursor = connection.cursor()
-
-        cursor.execute(f"CREATE DATABASE IF NOT EXISTS {database_name}")
-
-        print(f"Database '{database_name}' created successfully!")
-
-    except mysql.connector.Error as err:
-        print(f"Error: {err}")
-    finally:
-        cursor.close()
-        connection.close()
-
-create_database("hbtn_0c_0")
+-- Write a script that creates the database hbtn_0c_0 in MySQL server.
+-- If the database hbtn_0c_0 already exists, the script should not fail.
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
