e77cc44dc4c3db6a4b80a0a7876ec9cb2484a7a5
MamdouhEnahla <mamdouh57118@gmail.com>
2024-06-07 02:45:33 +0300

    0-body_size...

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 # use curl to send request and display response size
-
-url="$1"
-size=$(curl -sI "$url" | grep -i "content-length" | awk '{print $2}' | tr -d '\r\n')
-echo $size
+curl -s "$1" | wc -c
